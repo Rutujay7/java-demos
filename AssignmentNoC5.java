20997973ab5e7319569d3ec6fdc71f4dfd4b66a3
Rutujay7 <98747081+Rutujay7@users.noreply.github.com>
2022-02-05 18:37:57 +0530

    Add files via upload

---
+++ AssignmentNoC5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class AssignmentNoC5 {
+  public static void main(String[] args) {
+	  
+	  Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter the marks:");
+	  int n=sc.nextInt();
+	  
+	  if(n>=60) {
+		  
+		  System.out.println("Congratulations! You have cleared the exam");
+		  
+	  }
+		  
+	  else
+		  System.out.println("Sorry! You have not cleared the exam");
+	  
+	 
+}
+}
